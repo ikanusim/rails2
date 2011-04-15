d73da161eea78449c7b0a5df4c31fd46acf4759a
Daniel Hüsch <daniel.huesch@gmail.com>
2011-04-15 10:27:35 +0200

    wip

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
 ::File.open("/tmp/dhdbg-#{__FILE__.gsub('/', '_')}", 'w') do |file|
-  file.puts "dhdbg #{`whoami`}: #{Time.now}, #{__FILE__}, #{release_path}/config/database.yml='#{`cat #{release_path}/config/database.yml`}'"
+  file.puts "dhdbg #{`whoami`}: #{Time.now}, #{__FILE__}, #{release_path}/config/database.yml='#{`cat #{release_path}/config/database.yml 2>&1`}'"
+  file.puts "bundle check: #{`su deploy -c 'cd #{release_path} && bundle check 2>&1'`}"
 end
-sleep 5
