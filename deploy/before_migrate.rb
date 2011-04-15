50b74fc97bf71843caf16166c58eafea98cda228
Daniel Hüsch <daniel.huesch@gmail.com>
2011-04-15 16:00:30 +0200

    wip

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,4 @@
 ::File.open("/tmp/dhdbg-#{__FILE__.gsub('/', '_')}", 'w') do |file|
-  file.puts "su deploy -c 'cd #{release_path} && bundle install #{deploy[:home]}/.bundler/#{application} --without=test development'"
   file.puts "#{release_path.sub(/releases\/\d+/, 'shared')}/config/database.yml='#{`cat #{release_path.sub(/releases\/\d+/, 'shared')}/config/database.yml 2>&1`}'"
   file.puts "#{release_path}/config/database.yml='#{`cat #{release_path}/config/database.yml 2>&1`}'"
   file.puts "bundle check: #{`su deploy -c 'cd #{release_path} && bundle check 2>&1'`}"
