6d885bdc367146cfb7e4ddd48e554cf7c2baf322
Daniel Hüsch <daniel.huesch@gmail.com>
2011-04-15 09:32:44 +0200

    Make deploy hooks log to files.

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,4 @@
-puts "dhdbg: #{Time.now}, #{__FILE__}, #{release_path}/config/database.yml='`cat #{release_path}/config/database.yml`'"
+File.open("/tmp/dhdbg-#{__FILE__.gsub('/', '_')}", 'w') do |file|
+  file.puts "dhdbg: #{Time.now}, #{__FILE__}, #{release_path}/config/database.yml='`cat #{release_path}/config/database.yml`'"
+end
 sleep 5
