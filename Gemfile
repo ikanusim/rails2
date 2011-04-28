7487c7a951857ba7b60dec089958a219d31bc5db
Daniel Hüsch <daniel.huesch@gmail.com>
2011-04-28 07:48:39 +0200

    wip

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "http://rubygems.org"
 
+gem 'rails', '2.3.5'
+gem 'mysql'
 gem 'random'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  def index
+    render :text => <<-EO_INFO
+      Ruby: #{RUBY_VERSION rescue 'n/a'}
+      Rubygems: #{Gem::VERSION rescue 'n/a'}
+      Bundler: #{Bundler::VERSION rescue 'n/a'}
+      Passenger: #{PhusionPassenger::VERSION_STRING rescue 'n/a'}
+      Database adapter: #{ActiveRecord::Base.connection.adapter_name}
+      Rails root: #{Rails.root}
+    EO_INFO
+  end
 end
